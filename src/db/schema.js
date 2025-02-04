d874b2a96765ae218a7b11eb5139871ee7bf8e53
karthi582002 <karthi582002@gmail.com>
2025-02-04 16:51:53 +0530

    modified db schema

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,5 +1,5 @@
 import { mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
-import {boolean} from "drizzle-orm/pg-core";
+import {boolean, decimal, integer, timestamp} from "drizzle-orm/pg-core";
 
 export const users = mysqlTable("data", {
     id: serial("id").primaryKey(),
@@ -16,6 +16,7 @@ export const merchant_registration = mysqlTable("merchant_registration", {
     contactAltPhone: varchar("contactAltPhone",{length : 10}).notNull(),
     gstNumber: varchar("gstNumber",{length : 255}).unique().notNull(),
     email: varchar("email",{length : 100}).primaryKey().notNull(),
+    location:varchar("location",{length : 50}).notNull(),
     address: varchar("address",{length : 255}).notNull(),
     password: varchar("password",{length : 255}).notNull(),
     sellerDocuments: varchar("sellerDocument",{length : 255}).notNull(),
@@ -36,5 +37,22 @@ export const verified_merchant = mysqlTable("verified_merchant", {
     id: serial("id").autoincrement(),
     agent_email : varchar("agent_email",{length : 255}).notNull().references(()=> agent_registration.agent_email,{onDelete : "cascade"}),
     merchant_email : varchar("merchant_email",{length : 255}).notNull().references(()=> merchant_registration.email,{onDelete : "cascade"}),
+})
+
+export const registration_payment = mysqlTable("registration_payment",{
+    id: serial("id").primaryKey().autoincrement(),
+    orderId: varchar("order_id", {length: 255}).notNull(),
+    paymentId: varchar("payment_id",{length: 255}).notNull(),
+    email: varchar("email",{length: 255}).notNull().references(()=> merchant_registration.email,{onDelete : "cascade"}),
+    amount: decimal("amount").notNull(),
+    status: varchar("status",{length: 255}).default("PENDING"),
+    createdAt: timestamp("created_at").defaultNow(),
+})
+
+export const approved_merchant = mysqlTable("approved_merchant", {
+    merchantId: varchar("merchantId", {length: 8}).primaryKey(),
+    merchantEmail : varchar("merchantEmail",{length : 255}).notNull().references(()=> merchant_registration.email,{onDelete : "cascade"}),
+    password: varchar("password",{length : 255}).notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
 })
 
