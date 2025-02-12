4a991651bb955af9ee5df7c30f569a5f07a83df9
karthi582002 <karthi582002@gmail.com>
2025-02-12 11:04:06 +0530

    generate JWT Token for buyer

--- src/utils/generateToken..js
+++ src/utils/generateToken..js
@@ -47,3 +47,20 @@ export const generateAdminToken = (admin_email,res) =>{
         throw new Error('Token generation failed'); // Pass the error to the main controller
     }
 }
+
+export const generateBuyerToken = (buyer_email,res) =>{
+    try{
+            const token = jwt.sign({buyer_email: buyer_email},process.env.JWT_BUYER_SECRET,{expiresIn: '1hr'})
+            res.cookie('buyer_jwt',token,{
+                maxAge: 60*60*60*1000,
+                httpOnly: true,
+                sameSite: 'strict',
+                secure: process.env.NODE_ENV !== 'development',
+            });
+    }catch(err){
+        console.error(`Error generating buyer token: ${err.message}`);
+        res.status(500).json({
+            error : "Internal Server Error"
+        });
+    }
+}
