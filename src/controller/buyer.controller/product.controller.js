f40fc118596682855f50c64de836458dd208e6b1
karthi582002 <karthi582002@gmail.com>
2025-02-13 12:58:54 +0530

    get specific product completed

--- src/controller/buyer.controller/product.controller.js
+++ src/controller/buyer.controller/product.controller.js
@@ -14,3 +14,30 @@ export const fetchAllProducts = async(req,res)=>{
         res.status(500).send({
             error: "Internal Server Error",
         });
+
+    }
+}
+
+export const fetchSpecificProducts = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const productData = await getProductByProductId(id)
+        if(productData.length === 0){
+            return res.status(404).send({
+                error: "Product not found",
+            })
+        }
+        const productImages = await getProductImagesById(id)
+        console.log()
+        const finalData = {
+            productData: productData,
+            productImages: productImages[0].productImages,
+        };
+        res.status(200).json(finalData);
+    }catch(err){
+        console.log("Error while Fetching Specific Products : "+err);
+        res.status(500).send({
+            error: "Internal Server Error",
+        })
+    }
+}
