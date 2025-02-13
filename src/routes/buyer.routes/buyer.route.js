7c2d360d5ff12bbfe7f20962f08d51a34adc2138
karthi582002 <karthi582002@gmail.com>
2025-02-13 13:00:41 +0530

    added new routes for fetch all and single product

--- src/routes/buyer.routes/buyer.route.js
+++ src/routes/buyer.routes/buyer.route.js
@@ -4,5 +4,7 @@ import {buyerLogin, registerBuyer} from "../../controller/buyer.controller/buyer
 const router = express.Router();
 router.post("/register",registerBuyer)
 router.post("/login",buyerLogin)
+router.get("/all-products",fetchAllProducts)
+router.get("/product/:id",fetchSpecificProducts)
 
 export default router;
