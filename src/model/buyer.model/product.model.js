46c4208e73ed107c919c261e41af6508ee218b35
karthi582002 <karthi582002@gmail.com>
2025-02-13 13:00:06 +0530

    get all products model

--- src/model/buyer.model/product.model.js
+++ src/model/buyer.model/product.model.js
@@ -1,3 +1,26 @@
+import {product_skus, sku_resources} from "../../db/schema.js";
+import {eq} from "drizzle-orm";
+import {db} from "../../config/db.js";
+import {sql} from "drizzle-orm/sql/sql";
+
+export const getAllProducts = () => {
+    return db.select({
+        id : product_skus.id,
+        merchantId : product_skus.merchantId,
+        productId : product_skus.productId,
+        productGrade : product_skus.grade,
+        subGrade : product_skus.subGrade,
+        origin : product_skus.origin,
+        color : product_skus.color,
+        packing : product_skus.packing,
+        unitPrice : product_skus.unitPrice,
+        moisture : product_skus.moisture,
+        productImage :sql`MIN(${sku_resources.photoUrl})`.as("productImage"),
+    })
+        .from(sku_resources)
+        .leftJoin(product_skus,eq(product_skus.productId,sku_resources.productId))
+        .groupBy(product_skus.id)
+}
 
 export const getProductByProductId = (id) => {
     return db.select()
