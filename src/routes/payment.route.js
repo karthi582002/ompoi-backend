667c6b61e46eeea9aab9dbc4fd07c7dd6dfa9abc
karthi582002 <karthi582002@gmail.com>
2025-03-22 15:51:44 +0530

    added payment router for order section

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -2,7 +2,14 @@ import express from 'express';
 import {requestPayment, verifyPayment} from "../controller/payment.controller.js";
 
 const router = express.Router();
-router.post('/create-order',requestPayment)
-router.post('/verify-order',verifyPayment)
+// registration payment
+router.post('/create-registration-order',requestPayment)
+router.post('/verify-registration-order',verifyPayment)
+
+// Order Payment
+router.post('/create-order',protectedBuyerRoute,createOrderPayment)
+router.post('/verify-order',protectedBuyerRoute,verifyOrderPayment)
+
+
 
 export default router;
