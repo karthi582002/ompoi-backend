80613f40c2db983d469ad2d30503912a3ee606a4
karthi582002 <karthi582002@gmail.com>
2025-03-25 11:52:39 +0530

    Added Security Features cors,API keys

--- src/app.js
+++ src/app.js
@@ -16,17 +16,60 @@ import rateLimit from "express-rate-limit"
 dotenv.config();
 const app = express();
 
+// Rate limiter for preventing DDos Attack
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
     max: 10, // Limit each IP to 10 requests per minute
     message: "Too many requests, please try again later.",
 });
-// app.use(limiter);
-app.use(cors({
-    origin: "http://localhost:5173", // Frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true // Allow cookies if needed
-}));
+app.use(limiter);
+
+
+// Only Valid Users can connect with our backend
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    if (!allowedOrigins.includes(origin)) {
+        return res.status(403).json({ message: "Access Denied: Unauthorized Origin" });
+    }
+
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
+// // Even we cannot test our backend with help of postman this was comment out when in production it will works
+// app.use((req, res, next) => {
+//     const userAgent = req.headers["user-agent"] || "";
+
+//     // Block Postman and Curl requests
+//     if (userAgent.includes("Postman") || userAgent.includes("curl") || userAgent.includes("HTTPie")) {
+//         return res.status(403).json({ message: "Access Denied: Unauthorized Client" });
+//     }
+
+//     next();
+// });
+
+// Only With The secure API key only we can access our backend by the frontend
+app.use((req, res, next) => {
+    const apiKey = req.headers["x-api-key"];
+
+    if (apiKey !== process.env.API_SECRET_KEY) {
+        return res.status(403).json({ message: "Access Denied: Invalid API Key" });
+    }
+
+    next();
+});
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
